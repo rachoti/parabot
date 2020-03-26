8cf7bda72d8faa09f6c028962d01088240abc444
rachoti <rachoti.rk@gmail.com>
2020-03-26 11:25:21 +0530

    as

--- src/app/geomap/geomap.component.ts
+++ src/app/geomap/geomap.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit,Component, OnInit } from '@angular/core';
 import * as d3 from "d3";
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { GeomapService } from './geomap.service';
+import * as L from 'leaflet';
+
 declare var ol: any;
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 @Component({
   selector: 'app-geomap',
   templateUrl: './geomap.component.html',
   styleUrls: ['./geomap.component.css']
 })
 export class GeomapComponent implements OnInit {
-  latitude: number = 18.5204;
-  longitude: number = 73.8567;
-
-  map: any;
+  private map;
+  markers = [
+    {
+      "name": "Hubli",
+      "url": "https://en.wikipedia.org/wiki/Hubli",
+      "lat": 15.3173,
+      "lng": 75.7139
+    },
+    {
+      "name": "Anguilla",
+      "url": "https://en.wikipedia.org/wiki/Anguilla",
+      "lat": 18.220554,
+      "lng": -63.068615
+    },
+    {
+      "name": "Japan",
+      "url": "https://en.wikipedia.org/wiki/Japan",
+      "lat": 36.204824,
+      "lng": 138.252924
+    }
+ ];
   isShowDiv1 = true;  
   isShowDiv2 = true; 
   isShowDiv3 = true; 
@@ -63,48 +96,23 @@ export class GeomapComponent implements OnInit {
     
   }
 
-
   ngOnInit() {
-    
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([73.8567, 18.5204]),
-        zoom: 8
-      })
-    });
-    this.add_map_point(-33.8688, 151.2093);
+  var map = L.map( 'map', {
+      center: [20.0, 5.0],
+      minZoom: 2,
+      zoom: 2
+  });
+  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+  }).addTo( map );
+  for ( var i=0; i < this.markers.length; ++i ) 
+  {
+    L.marker( [this.markers[i].lat, this.markers[i].lng] )
+        .bindPopup( '<a href="' + this.markers[i].url + '" target="_blank">' + this.markers[i].name + '</a>' )
+        .addTo( map );
   }
-    add_map_point(lat, lng) {
-    var vectorLayer = new ol.layer.Vector({
-      source:new ol.source.Vector({
-        features: [new ol.Feature({
-              geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
-          })]
-      }),
-      style: new ol.style.Style({
-        image: new ol.style.Icon({
-          anchor: [0.5, 0.5],
-          anchorXUnits: "fraction",
-          anchorYUnits: "fraction",
-          src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
-        })
-      })
-    });
-
-
-  }
-  
-  setCenter() {
-    var view = this.map.getView();
-    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
-    view.setZoom(8);
-  }
+}
   
   logout(): void {
     console.log("Logout");
