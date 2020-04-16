a0158ac509563caca70e1f612cafc14af4696b06
aaa <your@email.com>
2020-04-16 18:42:26 +0530

    compare..

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -71,56 +71,134 @@ var yahooOnly1 = res.filter(function (entry) {
 final.push(yahooOnly,yahooOnly1)
 
 var fin=[final[0][0],final[1][0]]
-console.log(fin)
-    d3.select('svg').remove();
 
-    const element = this.chartContainer.nativeElement;
-    const data = fin;
+var lineData=[];
+for(let i=0;i<fin.length;i++){
+  var obj ={date:(new Date(fin[i].date).getFullYear())+"-0"+(new Date(fin[i].date).getMonth()+1)+"-"+(new Date(fin[i].date).getDate()-1),count:fin[i].count};
+  lineData.push(obj);
+}
+console.log(lineData.map(function(d) { return new Date(d.date); }))
+var height  = 400;
+var width   = 1000;
+var hEach   = 40;
 
-    const svg = d3.select(element).append('svg')
-      .attr('width', element.offsetWidth)
-      .attr('height', element.offsetHeight);
+var margin = {top: 15, right: 40, bottom: 20, left: 300};
 
-    const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
-    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
+width =     width - margin.left - margin.right;
+height =    height - margin.top - margin.bottom;
 
-    const x = d3
-      .scaleBand()
-      .rangeRound([0, contentWidth])
-      .padding(0.1)
-      .domain(data.map(d => d.date));
 
-    const y = d3
-      .scaleLinear()
-      .rangeRound([contentHeight, 0])
-      .domain([0, d3.max(data, d => d.count)]);
+//remove and create svg
+var buildlegendvalues =
+[{
+  "1.0": "#79abbd",
+  "2.0": "#6784b4",
+  
+}]
 
-    const g = svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  var bl = 0;
+  function getByIndex(buildlegendvalues, index) {
+    bl++
+    return buildlegendvalues[Object.keys(buildlegendvalues)[index]];
+  }
+var svg = d3.select('svg')
+.attr("width",  width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var x = d3.scaleBand()
+  .range([ 0, 500 ])
+  .domain(lineData.map(function(d) { return d.date; }))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(0,0)rotate(0)")
+    .style("text-anchor", "end");
 
-    g.append('g')
-      .attr('class', 'axis axis--x')
-      .attr('transform', 'translate(0,' + contentHeight + ')')
-      .call(d3.axisBottom(x));
+// Add Y axis
+var y = d3.scaleLinear()
+  .domain([0, 10])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y));
 
-    g.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3.axisLeft(y).ticks(10, '%'))
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .text('Frequency');
+// Bars
+svg.selectAll("mybar")
+  .data(lineData)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return x(d.date); })
+    .attr("y", function(d) { return y(d.count); })
+    .attr("width", 150)
+    .attr("height", function(d) { return height - y(d.count); })
+    .attr("fill", "#69b3a2")
+/*var x = d3.scaleTime().range([0, width]);
 
-    g.selectAll('.bar')
-      .data(data)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => x(d.date))
-      .attr('y', d => y(d.count))
-      .attr('width', x.bandwidth())
-      .attr('height', d => contentHeight - y(d.count));
+x.domain(d3.extent(fin, function(d) { return d.date; }));
+
+
+var y = d3.scaleLinear().range([height, 0]);
+
+
+y.domain([d3.min(fin, function(d) { return d.count; }) - 5, 1000]);
+
+svg.selectAll("myRect")
+    .lineData(fin)
+    .enter()
+    .append("rect")
+    .attr("x", x(0) )
+    .attr("y", function(d) { return y(d.date); })
+    .attr("width", function(d) { return x(d.count); })
+    .attr("height", y )
+    .attr("fill", "#69b3a2")
+/*var valueline = d3.line()
+      .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.count);  })
+      .curve(d3.curveMonotoneX);*/
+
+/*svg.append("rect")
+  .data([fin]) 
+  .attr("class", "bar")  
+.attr("d", valueline) 
+.attr("fill","none")
+.attr("stroke", "#ffab00")
+.attr("stroke-width", "3");
+
+//  var xAxis_woy = d3.axisBottom(x).tickFormat(d3.timeFormat("Week %V"));
+var xAxis_woy = d3.axisBottom(x).ticks(11).tickFormat(d3.timeFormat("%y-%b-%d")).tickValues(fin.map(d=>d.date));
+
+svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis_woy);
+
+//  Add the Y Axis
+//  svg.append("g").call(d3.axisLeft(y));
+
+svg.selectAll(".dot")
+  .data(fin)
+  .enter()
+  .append("circle") // Uses the enter().append() method
+  .attr("class", "dot") // Assign a class for styling
+  .attr("cx", function(d) { return x(d.date) })
+  .attr("cy", function(d) { return y(d.count) })
+  .attr("r", 5);  
+
+
+svg.selectAll(".text")
+  .data(fin)
+  .enter()
+  .append("text") // Uses the enter().append() method
+  .attr("class", "label") // Assign a class for styling
+  .attr("x", function(d, i) { return x(d.date) })
+  .attr("y", function(d) { return y(d.count) })
+  .attr("dy", "-5")
+  .text(function(d) {return d.count; });
+svg.append('text')                                     
+    .attr('x', 10)              
+    .attr('y', -5)    */
     });
   }
 
