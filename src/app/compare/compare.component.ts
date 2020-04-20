d563c824bf0035e5318fa42d48b9a5e7fe3dfb02
aaa <your@email.com>
2020-04-20 18:43:36 +0530

    compare_drop

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -12,6 +12,11 @@ export class CompareComponent implements OnInit {
   inputStartDate="";
   inputEndDate="";
   startDate1="";
+  startDate2="2017-01-01";
+  endDate2="";
+  inputStartDate2="";
+  inputEndDate2="";
+  startDate3="";
   datePicCount=0;
   @ViewChild('barChart')
   private chartContainer: ElementRef;
@@ -27,12 +32,16 @@ export class CompareComponent implements OnInit {
     this._httpService.getMessageCount().subscribe((res:any[])=>{
       this.startDate=""+(new Date(res[0].date).getFullYear())+"-0"+(new Date(res[0].date).getMonth()+1)+"-"+(new Date(res[0].date).getDate());
       this.endDate=""+(new Date(res[res.length-1].date).getFullYear())+"-0"+(new Date(res[res.length-1].date).getMonth()+1)+"-"+(new Date(res[res.length-1].date).getDate());
+      this.startDate2=""+(new Date(res[0].date).getFullYear())+"-0"+(new Date(res[0].date).getMonth()+1)+"-"+(new Date(res[0].date).getDate());
+      this.endDate2=""+(new Date(res[res.length-1].date).getFullYear())+"-0"+(new Date(res[res.length-1].date).getMonth()+1)+"-"+(new Date(res[res.length-1].date).getDate());
 
     });
 
   }
   dateChanger(startdate: string){
+    console.log(startdate)
     this.startDate1=startdate;
+    console.log(this.startDate1)
     this.datePicCount+=1;
   }
   dateChangerEnd(enddate: string){
@@ -40,44 +49,83 @@ export class CompareComponent implements OnInit {
     this.startDate1;
     this.inputStartDate=this.startDate1;
     this.inputEndDate=this.endDate;
-    this.createChart(this.startDate1,this.endDate);
+   // this.createChart(this.startDate1,this.endDate);
   }
+  dateChanger1(startdate: string){
+    this.startDate3=startdate;
+    this.datePicCount+=1;
+  }
+  dateChangerEnd1(enddate: string){
+    this.startDate1;
+    this.endDate;
+    this.endDate2=enddate;
+    this.startDate3;
+    this.inputStartDate2=this.startDate3;
+    this.inputEndDate2=this.endDate2;
+  this.createChart(this.startDate1,this.endDate,this.startDate3,this.endDate2);
+  }
+ myFunction(type) {
+   if(type=="new_user")
+   console.log("new user")
+   else  if(type=="ret_user")
+   console.log("ret user")
+   else  if(type=="message")
+   console.log("message")
     
-  private createChart(startDate,endDate): void {
-    var final=[]
+  }
+  private createChart(startDate1,endDate1,startDate2,endDate2): void {
+  //private createChart(): void{
+   /* var startDate1=this.startDate1;
+    var endDate1=this.endDate;
+    var startDate2=this.startDate3;
+    var endDate2=this.endDate2*/
+    var index1=0;
+    var index2=0;
     this._httpService.getMessageCount().subscribe((res:any[])=>{
+     for(var i=0;i<res.length;i++)
+     {
+      if((new Date(res[i].date).toLocaleDateString())==(new Date(startDate1).toLocaleDateString())){
+        break;
+     }
+      index1++;
+     }
+     for(var i=0;i<res.length;i++)
+     {
+      if((new Date(res[i].date).toLocaleDateString())==(new Date(startDate2).toLocaleDateString())){
+        break;
+     }
+      index2++;
+     }
+     var sum1=0;
+     var sum2=0;
+     for(var z=index1;(new Date(res[z].date))<=(new Date(endDate1)) ;z++)
+     {
+        sum1+=res[z].count;
+     }
+     console.log(sum1)
+     for(var a=index2;(new Date(res[a].date))<=(new Date(endDate2));a++)
+     {
+        sum2+=res[a].count;
+     }
+     console.log(sum2)
+ 
+
+    var final=[]
     
-    if (!res) { return; }
+    var line1={date:startDate1+"\t\tTO\t\t "+endDate1,count:sum1} 
+    var line2={date:startDate2+"\t\tTO\t\t "+endDate2,count:sum2} 
 
-  console.log(startDate)
-  var yahooOnly = res.filter(function (entry) {
-    var temp;
-    
-    temp=""+(new Date(entry.date).getFullYear())+"-0"+(new Date(entry.date).getMonth()+1)+"-"+(new Date(entry.date).getDate().toString().padStart(2, "0"));
-    console.log(temp)
-    return temp === startDate;
+final.push(line1,line2)
 
-    
-});
-var yahooOnly1 = res.filter(function (entry) {
-  var temp;
-  
-  temp=""+(new Date(entry.date).getFullYear())+"-0"+(new Date(entry.date).getMonth()+1)+"-"+(new Date(entry.date).getDate().toString().padStart(2, "0"));
-  console.log(temp)
-  return temp === endDate;
-
-  
-});
-final.push(yahooOnly,yahooOnly1)
-
-var fin=[final[0][0],final[1][0]]
+//var fin=[final[0][0],final[1][0]]
 
 var lineData=[];
-for(let i=0;i<fin.length;i++){
-  var obj ={date:(new Date(fin[i].date).getFullYear())+"-0"+(new Date(fin[i].date).getMonth()+1)+"-"+(new Date(fin[i].date).getDate()),count:fin[i].count};
+for(let i=0;i<final.length;i++){
+  var obj ={date:final[i].date,count:final[i].count};
+  console.log(obj)
   lineData.push(obj);
 }
-console.log(lineData.map(function(d) { return new Date(d.date); }))
+
 var height  = 400;
 var width   = 1000;
 var hEach   = 40;
@@ -85,7 +133,7 @@ var i=0;
 
 var myColor = d3.scaleOrdinal().domain(lineData[i=i+1])
   .range(["black", "grey"])
-var margin = {top: 15, right: 40, bottom: 20, left: 450};
+var margin = {top: 15, right: 40, bottom: 60, left: 450};
 
 width =     width - margin.left - margin.right;
 height =    height - margin.top - margin.bottom;
@@ -101,22 +149,30 @@ var svg = d3.select('#div_template')
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 var x = d3.scaleBand()
-  .range([ 0, 500 ])
+  .range([ 0, 400 ])
   .domain(lineData.map(function(d) { return d.date; }))
-  .padding(0.2);
+  .padding(0.2)
+  
 svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(0,0)rotate(0)")
-    .style("text-anchor", "end");
 
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x).tickSizeOuter(0))
+
+svg.append("g")
+.style("font-weight","bold")
 // Add Y axis
 var y = d3.scaleLinear()
   .domain([0, 10])
   .range([ height, 0]);
 svg.append("g")
-  .call(d3.axisLeft(y));
+  .call(d3.axisLeft(y))
+  .style("font-weight","bold");
+  svg.append("text")      // text label for the x axis
+        .attr("x", 205 )
+        .attr("y", 370 )
+        .style("text-anchor", "middle").
+        style("font-weight","bold")
+        .text("Date");
 
 // Bars
 var Tooltip = d3.select('#div_template')
@@ -171,13 +227,14 @@ svg.selectAll("mybar")
     .attr("y",15) // 100 is where the first dot appears. 25 is the distance between dots
     .attr("width", size)
     .attr("height", size).style("fill", "black")
+    
     svg.append("rect").
     attr("x", 420)
     .attr("y",45) // 100 is where the first dot appears. 25 is the distance between dots
     .attr("width", size)
     .attr("height", size).style("fill", "grey")
-    svg.append("text").attr("x", 460).attr("y", 30).text("Bot1").style("font-size", "20px").attr("alignment-baseline","right")
-    svg.append("text").attr("x", 460).attr("y", 60).text("Bot2").style("font-size", "20px").attr("alignment-baseline","right")
+    svg.append("text").attr("x", 460).attr("y", 30).text("Bot1").style("font-size", "20px").style("font-weight","bold").attr("alignment-baseline","right")
+    svg.append("text").attr("x", 460).attr("y", 60).text("Bot2").style("font-size", "20px").style("font-weight","bold").attr("alignment-baseline","right")
 /*var x = d3.scaleTime().range([0, width]);
 
 x.domain(d3.extent(fin, function(d) { return d.date; }));
@@ -243,7 +300,8 @@ svg.selectAll(".text")
 svg.append('text')                                     
     .attr('x', 10)              
     .attr('y', -5)    */
-    });
+});
+   
   }
 
 }
