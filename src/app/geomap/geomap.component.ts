9a52bf1f9140f4040050593043003e42e8051b0c
aaa <your@email.com>
2020-04-30 16:16:59 +0530

    FINALLLY

--- src/app/geomap/geomap.component.ts
+++ src/app/geomap/geomap.component.ts
@@ -32,9 +32,47 @@ export class GeomapComponent implements OnInit {
   actdata2="+ Compare";
   actdata3="+ Conversation";
   actdata4="+ Demographics";
+
   thData="New";
   d1;
   d2;
+  toggleDisplayDiv1() {
+    this.isShowDiv1 = !this.isShowDiv1;
+    if(this.actdata1==="+Activity"){
+      this.actdata1="-Activity";
+    }else{
+      this.actdata1="+Activity";
+    }
+    
+  }
+  
+  toggleDisplayDiv2() {
+    this.isShowDiv2 = !this.isShowDiv2;
+    if(this.actdata2==="+Compare"){
+      this.actdata2="-Compare";
+    }else{
+      this.actdata2=" +Compare";
+    }
+    
+  }
+
+  toggleDisplayDiv3() {
+    this.isShowDiv3= !this.isShowDiv3;
+    if(this.actdata3==="+Conversation"){
+      this.actdata3="- Conversation";
+    }else{
+      this.actdata3="+ Conversation";
+    }
+    
+  }
+  toggleDisplayDiv4() {
+    this.isShowDiv4= !this.isShowDiv4;
+    if(this.actdata4==="+Demographics"){
+      this.actdata4="-Demographics";
+    }else{
+      this.actdata4="+Demographics";
+    }
+  }
   private ui: any;
   private search: any;
   @ViewChild("map")
