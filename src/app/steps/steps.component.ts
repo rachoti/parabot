1e066af25ddfd1a942889bdfd327c13123eca34a
Nikhitha Shetty <nikhitha.shetty@slksoft.com>
2020-06-23 17:29:12 +0530

    steps_csv

--- src/app/steps/steps.component.ts
+++ src/app/steps/steps.component.ts
@@ -3,7 +3,7 @@ import { GeomapComponent } from '../geomap/geomap.component';
 import { attachEmbeddedView } from '@angular/core/src/view';
 import { template } from '@angular/core/src/render3';
 import { forEach } from '@angular/router/src/utils/collection';
-
+import { ExportToCsv } from 'export-to-csv';
 //import { getMaxListeners } from 'cluster';
 declare var $:any;
 
@@ -13,6 +13,7 @@ declare var $:any;
   styleUrls: ['./steps.component.css']
 })
 export class StepsComponent implements OnInit {
+	finall=[]
 	bb;
 	aa=[];
 	ar;
@@ -93,7 +94,7 @@ var col_index=[]
 		{
 			this.filter_data.push(neww[i])
 		}
-		var finall=[]
+	
 		for(let i=1;i<ll.length;i++)
 		{
 			temp=[]
@@ -120,9 +121,31 @@ var col_index=[]
 			
 	
 		console.log(lineData)
-
-
+		
+		
+		for(let i=0;i<lineData.length;i++)
+		{
+			var output={}
+		for(let j=0;j<lineData[i].length;j++)
+		{
+		
+		for (var key in lineData[i][j]) {
+			output[key] = lineData[i][j][key];
+			
+		   }
+		   
 		}
+		   
+		   this.finall.push(output)
+		   
+		}
+		
+		
+		   
+			
+		  
+		}
+		
 			
 
   ngOnInit() {
@@ -136,6 +159,7 @@ var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function(){
+	
 	if(animating) return false;
 	animating = true;
 	
@@ -219,6 +243,23 @@ $(".submit").click(function(){
 	this.isModalThreeVisible=false;
 }
 onclick1(){
+	const options = { 
+		fieldSeparator: ',',
+		quoteStrings: '"',
+		decimalSeparator: '.',
+		showLabels: true, 
+		showTitle: true,
+		title: '',
+		useTextFile: false,
+		useBom: true,
+		useKeysAsHeaders: true,
+		// headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+	  };
+	 
+	const csvExporter = new ExportToCsv(options);
+	 
+   csvExporter.generateCsv(this.finall);
+	
 	this.isModalOneVisible=false;
 	this.isModalTwoVisible=false;
 	this.isModalThreeVisible=true;
