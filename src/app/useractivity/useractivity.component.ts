7be7434b07a60baa192bbc24cf54850234ad87a7
Nikhitha Shetty <nikhitha.shetty@slksoft.com>
2020-07-24 12:14:59 +0530

    single_date

--- src/app/useractivity/useractivity.component.ts
+++ src/app/useractivity/useractivity.component.ts
@@ -398,13 +398,34 @@ else if(type=="new" && this.startDate1!="")
      }
       index++;
     }
-    var lineData = [];
+    var lineData1 = [];
     for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
       var obj ={date:new Date(res[z].Date),user_count:res[z].count_users};
       console.log(obj)
-      lineData.push(obj);
+      lineData1.push(obj);
     }
-   
+    var lineData2=[]
+    var lineData=[]
+    if(lineData1.length>7)
+    {
+    for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+      var s=lineData1[j]
+      lineData2.push(s);
+    }
+    
+    for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+      var s=lineData2[j]
+      lineData.push(s);
+      }
+    }
+    else if(lineData1.length<=7)
+    {
+      for(var j=0;j<lineData1.length;j++)
+      {
+        var s=lineData1[j]
+        lineData.push(s)
+      }
+    }
     var height  = 400;
  var width   = 1000;
  var hEach   = 40;
@@ -503,13 +524,34 @@ else if(type=="ret" && this.startDate1!="")
      }
       index++;
     }
-    var lineData = [];
+    var lineData1 = [];
     for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
       var obj ={date:new Date(res[z].date),user_count:res[z].count};
       console.log(obj)
-      lineData.push(obj);
+      lineData1.push(obj);
     }
-   
+    var lineData2=[]
+    var lineData=[]
+    if(lineData1.length>7)
+    {
+    for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+      var s=lineData1[j]
+      lineData2.push(s);
+    }
+    
+    for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+      var s=lineData2[j]
+      lineData.push(s);
+      }
+    }
+    else if(lineData1.length<=7)
+    {
+      for(var j=0;j<lineData1.length;j++)
+      {
+        var s=lineData1[j]
+        lineData.push(s)
+      }
+    }
     var height  = 400;
  var width   = 1000;
  var hEach   = 40;
@@ -606,13 +648,34 @@ var date_diff_indays = function(date1, date2) {
    }
     index++;
   }
-  var lineData = [];
+  var lineData1 = [];
   for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
     var obj ={date:new Date(res[z].date),user_count:res[z].count};
     console.log(obj)
-    lineData.push(obj);
+    lineData1.push(obj);
   }
- 
+  var lineData2=[]
+  var lineData=[]
+  if(lineData1.length>7)
+  {
+  for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+    var s=lineData1[j]
+    lineData2.push(s);
+  }
+  
+  for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+    var s=lineData2[j]
+    lineData.push(s);
+    }
+  }
+  else if(lineData1.length<=7)
+  {
+    for(var j=0;j<lineData1.length;j++)
+    {
+      var s=lineData1[j]
+      lineData.push(s)
+    }
+  }
   var height  = 400;
 var width   = 1000;
 var hEach   = 40;
@@ -699,14 +762,34 @@ svg.append('text')
 
   } 
   dateChangerEnd(enddate: string){
-    console.log(this.type1)
+  
   this.endDate=enddate;
-  this.startDate1;
-  console.log()
-  this.inputStartDate=this.startDate1;
+  this._httpService.getRetUserCount().subscribe((res:any[])=>{
+    this.startDate1= ""+(new Date(res[0].date).getFullYear())+"-0"+(new Date(res[0].date).getMonth()+1)+"-"+(new Date(res[0].date).getDate());
+    var b;
+    for(var j=res.length-1;j>=0;j--)
+    {
+      if(new Date(res[j].date).toLocaleDateString()==new Date(this.endDate).toLocaleDateString())
+      {
+        b=j-6;
+      }
+    }
+    console.log("aaaaaaaaaaaaaaa")
+    if(b>=0)
+    {
+    this.inputStartDate=""+(new Date(res[b].date).getFullYear())+"-0"+(new Date(res[b].date).getMonth()+1)+"-"+(new Date(res[b].date).getDate());;
+    }
+    else if(b<0)
+    {
+      this.inputStartDate=this.startDate1;
+    }
+    this.inputEndDate=this.endDate;
+    });
+    
   this.inputEndDate=this.endDate;
   if(this.type1=='new' || this.type1==null)
   {
+    
   document.getElementById("divv").innerHTML=" ";
   var date_diff_indays = function(date1, date2) {
     
@@ -727,13 +810,37 @@ svg.append('text')
      }
       index++;
     }
-    var lineData = [];
+    
+    var lineData1= [];
     for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
       var obj ={date:new Date(res[z].Date),user_count:res[z].count_users};
       console.log(obj)
-      lineData.push(obj);
+      lineData1.push(obj);
     }
-   
+    var lineData2=[]
+    var lineData=[]
+    if(lineData1.length>7)
+    {
+    for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+      var s=lineData1[j]
+      lineData2.push(s);
+    }
+    
+    for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+      var s=lineData2[j]
+      lineData.push(s);
+      }
+    }
+    else if(lineData1.length<=7)
+    {
+      for(var j=0;j<lineData1.length;j++)
+      {
+        var s=lineData1[j]
+        lineData.push(s)
+      }
+    }
+    console.log(lineData)
+   console.log(lineData.length)
     var height  = 400;
  var width   = 1000;
  var hEach   = 40;
@@ -813,6 +920,7 @@ svg.append('text')
   }
   else if(this.type1=='ret')
   {
+    console.log(this.startDate1)
     document.getElementById("divv").innerHTML=" ";
   var date_diff_indays = function(date1, date2) {
     
@@ -833,11 +941,33 @@ svg.append('text')
      }
       index++;
     }
-    var lineData = [];
+    var lineData1 = [];
     for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
       var obj ={date:new Date(res[z].date),user_count:res[z].count};
       console.log(obj)
-      lineData.push(obj);
+      lineData1.push(obj);
+    }
+    var lineData2=[]
+    var lineData=[]
+    if(lineData1.length>7)
+    {
+    for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+      var s=lineData1[j]
+      lineData2.push(s);
+    }
+    
+    for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+      var s=lineData2[j]
+      lineData.push(s);
+      }
+    }
+    else if(lineData1.length<=7)
+    {
+      for(var j=0;j<lineData1.length;j++)
+      {
+        var s=lineData1[j]
+        lineData.push(s)
+      }
     }
    
     var height  = 400;
@@ -917,108 +1047,133 @@ svg.append('text')
   }
   else if(this.type1=='tot')
   {
+    
+    console.log(this.endDate)
     document.getElementById("divv").innerHTML=" ";
-  var date_diff_indays = function(date1, date2) {
-    
-    let dt1 = new Date(date1);
-    
-    let dt2 = new Date(date2);
-    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
-    }
-    this._httpService.getUserCount().subscribe((res:any[])=>{
-      var index=0
+var date_diff_indays = function(date1, date2) {
   
-    for(var j=0;j<res.length;j++){
-      
-      //console.log(new Date(this.startDate1))
-      //console.log(new Date(yahooOnly[j].date).toLocaleDateString())
-     if((new Date(res[j].date).toLocaleDateString())==(new Date(this.startDate1).toLocaleDateString())){
-        break;
-     }
-      index++;
-    }
-    var lineData = [];
-    for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
-      var obj ={date:new Date(res[z].date),user_count:res[z].count};
-      console.log(obj)
-      lineData.push(obj);
-    }
-   
-    var height  = 400;
- var width   = 1000;
- var hEach   = 40;
-
- var margin = {top: 15, right: 40, bottom: 20, left: 30};
-
- width =     width - margin.left - margin.right;
- height =    height - margin.top - margin.bottom;
-
- var svg = d3.select('#divv')
- .append("svg")
-   .attr("width",  width + margin.left + margin.right)
-   .attr("height", height + margin.top + margin.bottom)
-   .append("g")
-   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
- var x = d3.scaleTime().range([0, width]);
+  let dt1 = new Date(date1);
   
- x.domain(d3.extent(lineData, function(d) { return d.date; }));
+  let dt2 = new Date(date2);
+  return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+  }
+  this._httpService.getUserCount().subscribe((res:any[])=>{
+    this.startDate1= ""+(new Date(res[0].date).getFullYear())+"-0"+(new Date(res[0].date).getMonth()+1)+"-"+(new Date(res[0].date).getDate());
+
+    var index=0
+
+  for(var j=0;j<res.length;j++){
+    
+    //console.log(new Date(this.startDate1))
+    //console.log(new Date(yahooOnly[j].date).toLocaleDateString())
+   if((new Date(res[j].date).toLocaleDateString())==(new Date(this.startDate1).toLocaleDateString())){
+      break;
+   }
+    index++;
+  }
+  var lineData1 = [];
+  for(var z=index;z<=date_diff_indays(this.startDate1,this.endDate)+index;z++){
+    var obj ={date:new Date(res[z].date),user_count:res[z].count};
+    console.log(obj)
+    lineData1.push(obj);
+  }
+  var lineData2=[]
+  var lineData=[]
+  if(lineData1.length>7)
+  {
+  for(var j=lineData1.length-1;j>=(lineData1.length-7);j--){
+    var s=lineData1[j]
+    lineData2.push(s);
+  }
+  
+  for(var j=lineData2.length-1;j>=(lineData2.length-7);j--){
+    var s=lineData2[j]
+    lineData.push(s);
+    }
+  }
+  else if(lineData1.length<=7)
+  {
+    for(var j=0;j<lineData1.length;j++)
+    {
+      var s=lineData1[j]
+      lineData.push(s)
+    }
+  }
+  var height  = 400;
+var width   = 1000;
+var hEach   = 40;
+
+var margin = {top: 15, right: 40, bottom: 20, left: 30};
+
+width =     width - margin.left - margin.right;
+height =    height - margin.top - margin.bottom;
+
+var svg = d3.select('#divv')
+.append("svg")
+ .attr("width",  width + margin.left + margin.right)
+ .attr("height", height + margin.top + margin.bottom)
+ .append("g")
+ .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleTime().range([0, width]);
+
+x.domain(d3.extent(lineData, function(d) { return d.date; }));
 
 
- var y = d3.scaleLinear().range([height, 0]);
+var y = d3.scaleLinear().range([height, 0]);
 
 
- y.domain([d3.min(lineData, function(d) { return d.user_count; }) , 10]);
+y.domain([d3.min(lineData, function(d) { return d.user_count; }) , 10]);
 
- var valueline = d3.line()
-         .x(function(d) { return x(d.date); }) 		.y(function(d) { return y(d.user_count);  })
-         .curve(d3.curveMonotoneX);
+var valueline = d3.line()
+       .x(function(d) { return x(d.date); }) 		.y(function(d) { return y(d.user_count);  })
+       .curve(d3.curveMonotoneX);
 
- svg.append("path")
-     .data([lineData]) 
-     .attr("class", "line")  
- 	.attr("d", valueline) 
- 	.attr("fill","none")
- 	.attr("stroke", "#ffab00")
- 	.attr("stroke-width", "3");
+svg.append("path")
+   .data([lineData]) 
+   .attr("class", "line")  
+ .attr("d", valueline) 
+ .attr("fill","none")
+ .attr("stroke", "#ffab00")
+ .attr("stroke-width", "3");
 
-  var xAxis_woy = d3.axisBottom(x).tickFormat(d3.timeFormat("Week %V"));
- var xAxis_woy = d3.axisBottom(x).ticks(11).tickFormat(d3.timeFormat("%y-%b-%d")).tickValues(lineData.map(d=>d.date));
+var xAxis_woy = d3.axisBottom(x).tickFormat(d3.timeFormat("Week %V"));
+var xAxis_woy = d3.axisBottom(x).ticks(11).tickFormat(d3.timeFormat("%y-%b-%d")).tickValues(lineData.map(d=>d.date));
 
- svg.append("g")
-         .attr("class", "x axis")
-         .attr("transform", "translate(0," + height + ")")
-         .call(xAxis_woy);
+svg.append("g")
+       .attr("class", "x axis")
+       .attr("transform", "translate(0," + height + ")")
+       .call(xAxis_woy);
 
-  //Add the Y Axis
-  svg.append("g").call(d3.axisLeft(y));
+//Add the Y Axis
+svg.append("g").call(d3.axisLeft(y));
 
- svg.selectAll(".dot")
-     .data(lineData)
-     .enter()
-    .append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-     .attr("cx", function(d) { return x(d.date) })
-     .attr("cy", function(d) { return y(d.user_count) })
-     .attr("r", 5);  
-
-
- svg.selectAll(".text")
+svg.selectAll(".dot")
    .data(lineData)
-    .enter()
-    .append("text") // Uses the enter().append() method
-    .attr("class", "label") // Assign a class for styling    
-     .attr("x", function(d, i) { return x(d.date) })
-    .attr("y", function(d) { return y(d.user_count) })
-    .attr("dy", "-5")
-    .text(function(d) {return d.user_count; });
+   .enter()
+  .append("circle") // Uses the enter().append() method
+  .attr("class", "dot") // Assign a class for styling
+   .attr("cx", function(d) { return x(d.date) })
+   .attr("cy", function(d) { return y(d.user_count) })
+   .attr("r", 5);  
 
- svg.append('text')                                     
-       .attr('x', 10)              
-       .attr('y', -5)    
-       
-  
-    });
+
+svg.selectAll(".text")
+ .data(lineData)
+  .enter()
+  .append("text") // Uses the enter().append() method
+  .attr("class", "label") // Assign a class for styling    
+   .attr("x", function(d, i) { return x(d.date) })
+  .attr("y", function(d) { return y(d.user_count) })
+  .attr("dy", "-5")
+  .text(function(d) {return d.user_count; });
+
+svg.append('text')                                     
+     .attr('x', 10)              
+     .attr('y', -5)    
+     
+
+  });
   }
   }
   constructor(private router: Router,public authService: AuthService,  private _httpService:UseravtivityService) { 
